26519df2350f92327ac3dfc413a6d12e8ebb0438
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-07 16:29:56 +0900

    brand new script added

--- script/log.sh
+++ script/log.sh
@@ -10,17 +10,31 @@ keys   = ["--start", "--shutdown"]
 home   = File.expand_path('~')
 dest   = "#{home}/.log/log.csv"
 
+def show_help_menu()
+    appname = "logh"
+    puts
+    puts "#{appname} [-h|help]  ... show help menu"
+    puts "#{appname} --start    ... write start script   (起動)"
+    puts "#{appname} --shutdown ... write shutdown script(終了)"
+    puts
+    abort
+end
+
+is_help = ["-h", "help"].include?(option)
+
+if is_help
+    show_help_menu
+end
+
 if ! keys.include?(option)
-    puts "Your argument is wrong. Please input either #{keys[0]} or #{keys[1]}"
+    puts "\nYour argument is wrong. Please input either #{keys[1]} or #{keys[1]}\n\n"
     abort
 end
 
 maps = {
     "--start"    => "起動",
     "--shutdown" => "終了"
-
 }
-
 date_string = DateTime.now.strftime("%Y/%d/%m %H:%M:%S #{maps[option]}")
 
 File.open(dest, "a+") do |f| 
