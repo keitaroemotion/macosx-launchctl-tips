4e6b34270b4f4325ce812ab04960d755d00f3bb3
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-08 13:01:40 +0900

    XX

--- script/log.sh
+++ script/log.sh
@@ -6,12 +6,12 @@ option     = ARGV[0]
 keys       = ["--start", "--shutdown"]
 home       = File.expand_path('~')
 dest       = "#{home}/.log/log.csv"
-dest_cache = "#{home}/.log/.log.csv"
 
 def show_help_menu()
     appname = "logh"
     puts
     puts "#{appname} [-h|help]  ... show help menu"
+    puts "#{appname} --show     ... show logs"
     puts "#{appname} --start    ... write start script   (起動)"
     puts "#{appname} --shutdown ... write shutdown script(終了)"
     puts
@@ -19,13 +19,19 @@ def show_help_menu()
 end
 
 is_help = ["-h", "help"].include?(option)
+is_show = "--show" == option
 
 if is_help
     show_help_menu
 end
 
+if is_show
+    system "cat #{dest}"
+    abort
+end
+
 if ! keys.include?(option)
-    puts "\nYour argument is wrong. Please input either #{keys[1]} or #{keys[1]}\n\n"
+    puts "\nYour argument is wrong. Please input either #{keys[0]} or #{keys[1]}\n\n"
     abort
 end
 
@@ -40,4 +46,4 @@ File.open(dest, "a+") do |f|
     f.puts "#{date_string}"
 end
 
-puts "-------------------- DONE --------------------"
+puts "\nLogging complete.\n\n"
