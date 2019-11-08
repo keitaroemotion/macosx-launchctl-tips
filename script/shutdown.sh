c5e0229a3834c6aff767fd261781a7873fd45116
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-08 17:02:07 +0900

    launching startup script success (with idempotency)

--- script/shutdown.sh
+++ script/shutdown.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# mkdir -p /usr/local/etc/logh
-# dest_path=/usr/local/etc/logh/shutdown.log
-# echo "END: $(date)" >> $dest_path
-# cat $dest_path
-$HOME/code/logh/script/log.sh --shutdown
+loghive --shutdown
--- script/startup.sh
+++ script/startup.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-# mkdir -p /usr/local/etc/logh
-# dest_path=/usr/local/etc/logh/startup.log
-# echo $(date) >> $dest_path
-# cat $dest_path
-echo $HOME/code/logh/script/log.sh --start
-$HOME/code/logh/script/log.sh --start
+loghive --start
